099bf377e04ed7e7ae6ecb87cafc678586d6b960
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-03-15 13:17:47 -0300

    refactor apiRequest function to include ApiResponse interface for better response handling

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -1,10 +1,16 @@
+interface ApiResponse<T> {
+    status: number;
+    message: string;
+    data?: T;
+}
+
 export async function apiRequest<T>(
     endpoint: string,
     method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
     body?: object
 ): Promise<T> {
     try {
-        const response = await fetch(`api/${endpoint}`, {
+        const resquest = await fetch(`api/${endpoint}`, {
             method,
             headers: {
                 "Content-Type": "application/json",
@@ -12,11 +18,20 @@ export async function apiRequest<T>(
             body: method !== "GET" && body ? JSON.stringify(body) : undefined,
         });
 
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+        if (!resquest.ok) {
+            throw new Error(`Erro: ${resquest.status} - ${resquest.statusText}`);
         }
 
-        return await response.json();
+        const apiResponse = await resquest.json();
+
+        const response: ApiResponse<T> = {
+            status: apiResponse.status,
+            message: apiResponse.message,
+            data: apiResponse.data,
+        };
+
+        return response as T;
+
     } catch (error) {
         console.error("Erro na requisição:", error);
         throw error;
