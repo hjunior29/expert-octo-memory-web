87ecd62f37b1d139b4d9b91f06fbbe0b8172735a
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-03-24 23:47:58 -0300

    feat: implement modal for generating flashcards with file upload and input options

--- src/routes/(private)/folder/[folderId]/+page.svelte
+++ src/routes/(private)/folder/[folderId]/+page.svelte
@@ -4,7 +4,13 @@
     import type { Notification } from "$lib/models/notification";
     import {
         Button,
+        FileUploader,
         Modal,
+        ProgressBar,
+        Tab,
+        TabContent,
+        Tabs,
+        TextArea,
         TextInput,
         ToastNotification,
     } from "carbon-components-svelte";
@@ -14,6 +20,9 @@
     import type { Folder } from "$lib/models/folder";
     import { page } from "$app/state";
     import FlashcardIcon from "$lib/components/custom/FlashcardIcon.svelte";
+    import { AiGenerate } from "carbon-icons-svelte";
+    import type { Flashcard, FlashcardGenerate } from "$lib/models/flashcard";
+    import { fileToBase64 } from "$lib/utils";
 
     type TopicAndFlashcards = {
         folder: Folder;
@@ -23,15 +32,22 @@
     let folder: Folder = {};
     let topic: Topic = {};
     let topics: Topic[] = [];
+    let flashcardGenerate: FlashcardGenerate = {};
+    let fileUploader;
+    let files: File[] = [];
     let notification: Notification = {};
     let timeout: any = undefined;
+    let openGenerateFlashcardsModal: boolean = false;
     let openEditTopicModal: boolean = false;
+    let isGeneratingFlashcards: boolean = false;
 
     let folderId = page.params.folderId;
 
     onMount(() => {
         getTopics();
     });
+
+    async function createTopic() {}
 
     async function getTopics() {
         const response = await apiRequest<ApiResponse<TopicAndFlashcards>>(
@@ -114,14 +130,53 @@
         getTopics();
     }
 
-    function handleEditTopic({
-        topicId,
-        name,
-    }: {
-        topicId: string;
-        name: string;
-    }) {
-        folder = { id: Number(topicId), name };
+    async function generateFlashcards() {
+        let dataType: string = "";
+
+        if (flashcardGenerate.text) {
+            dataType = "text";
+        } else if (flashcardGenerate.link) {
+            dataType = "link";
+        } else if (flashcardGenerate.topic) {
+            dataType = "topic";
+        } else if (files.length) {
+            dataType = "file";
+        }
+
+        openGenerateFlashcardsModal = false;
+        isGeneratingFlashcards = true;
+
+        const response = await apiRequest<ApiResponse<Flashcard[]>>(
+            "flashcards/generate/" + dataType + "/" + folderId,
+            "POST",
+            flashcardGenerate,
+        );
+
+        if (response.status === 201) {
+            isGeneratingFlashcards = false;
+            notification = {
+                kind: "success",
+                title: "Sucesso",
+                subtitle: response.message,
+                caption: new Date().toLocaleString(),
+                timeout: 3_000,
+            };
+        } else {
+            isGeneratingFlashcards = false;
+            notification = {
+                kind: "error",
+                title: "Erro",
+                subtitle: response.message,
+                caption: new Date().toLocaleString(),
+                timeout: 3_000,
+            };
+        }
+
+        getTopics();
+    }
+
+    function handleEditTopic(updatedTopic: { topicId: number; name: string }) {
+        topic = { id: updatedTopic.topicId, name: updatedTopic.name };
         openEditTopicModal = true;
     }
 
@@ -129,15 +184,28 @@
         topic = { id: topicId };
         deleteTopic(topicId);
     }
+
+    async function handleFileUpload() {
+        if (files.length > 0) {
+            const base64Files = await Promise.all(
+                files.map((file) => fileToBase64(file)),
+            );
+            flashcardGenerate.files = base64Files.map((file) => ({
+                base64: file,
+            }));
+        }
+    }
 </script>
 
 <div class="flex justify-end">
     <Button
+        icon={AiGenerate}
         on:click={() => {
-            openEditTopicModal = true;
-        }}>Criar nova pasta</Button
+            openGenerateFlashcardsModal = true;
+        }}>Gerar Flashcards</Button
     >
 </div>
+
 <div class="mt-10">
     <div class="w-full flex flex-wrap gap-8">
         {#each topics as _, i}
@@ -151,6 +219,91 @@
         {/each}
     </div>
 </div>
+
+<Modal
+    bind:open={openGenerateFlashcardsModal}
+    modalHeading="Gerar Flashcards"
+    primaryButtonIcon={AiGenerate}
+    primaryButtonText="Gerar"
+    secondaryButtonText="Cancelar"
+    on:click:button--secondary={() => (openGenerateFlashcardsModal = false)}
+    on:open
+    on:close
+    on:submit={generateFlashcards}
+>
+    <p>
+        Você pode gerar flashcards a partir de links de sites, vídeos do
+        YouTube, vídeos enviados à plataforma ou apenas descrevendo o assunto
+        que deseja.
+    </p>
+    <br />
+    <Tabs type="container">
+        <Tab label="Texto" />
+        <Tab label="Link" />
+        <Tab label="Assunto" />
+        <Tab label="Arquivo" />
+        <svelte:fragment slot="content">
+            <TabContent>
+                <div>
+                    <p>Cole o texto de um determinado assunto.</p>
+                </div>
+                <TextArea
+                    invalidText="Texto é obrigatório."
+                    labelText="Texto *"
+                    placeholder="Cole o texto..."
+                    bind:value={flashcardGenerate.text}
+                />
+            </TabContent>
+            <TabContent>
+                <div>
+                    <p>Cole o link de um site ou vídeo.</p>
+                </div>
+                <TextInput
+                    invalidText="Link é obrigatório."
+                    labelText="Link *"
+                    placeholder="Cole o link..."
+                />
+            </TabContent>
+            <TabContent>
+                <div>
+                    <p>Escreva sobre o assunto.</p>
+                </div>
+                <TextInput
+                    invalidText="Texto é obrigatório."
+                    labelText="Texto"
+                    placeholder="Escreva sobre o assunto..."
+                />
+            </TabContent>
+            <TabContent>
+                <div>
+                    <p>Envie um PDF, imagem, DOCX ou áudio MP3.</p>
+                </div>
+                <FileUploader
+                    bind:this={fileUploader}
+                    multiple
+                    labelTitle="Upload files"
+                    buttonLabel="Adicionar arquivo"
+                    labelDescription="Clique para adicionar"
+                    accept={[".pdf", ".docx", ".jpg", ".jpeg", ".png", ".mp3"]}
+                    status="complete"
+                    bind:files
+                    on:add={handleFileUpload}
+                />
+                {#if files.length}
+                    <br />
+                    <Button
+                        size="small"
+                        kind="tertiary"
+                        disabled={!files.length}
+                        on:click={fileUploader.clearFiles}
+                    >
+                        Limpar
+                    </Button>
+                {/if}
+            </TabContent>
+        </svelte:fragment>
+    </Tabs>
+</Modal>
 
 <Modal
     bind:open={openEditTopicModal}
@@ -189,5 +342,25 @@
                 notification = {};
             }}
         />
+    </div>
+{/if}
+
+{#if isGeneratingFlashcards}
+    <div
+        class="fixed bottom-4 right-4 w-96 transition-opacity duration-300"
+        transition:fade
+    >
+        <ToastNotification
+            timeout={notification.timeout}
+            kind="info"
+            title="Gerando Flashcards..."
+            caption={new Date().toLocaleString()}
+            hideCloseButton={true}
+            on:close={(e) => {
+                timeout = undefined;
+                notification = {};
+            }}
+        />
+        <ProgressBar size="sm" helperText="Gerando..." class="w-11/12" />
     </div>
 {/if}
